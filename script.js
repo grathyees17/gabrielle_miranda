add11859c5afcb07b70a5927bcf53367b00d4526
MicaelCosmo <micael.trabalho@hotmail.com>
2025-11-18 21:35:00 -0300

    feat: first release of the resume

---
+++ script.js
@@ -0,0 +1,4 @@
+// O design atual é estático e centralizado. O script de filtragem não é utilizado.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Currículo Jovem Aprendiz carregado com layout centralizado e elegante.");
+});
